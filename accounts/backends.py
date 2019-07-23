87d36cc0e2e2e1335cabe10ead2b346372065a59
Shane Muirhead <shane_muirhead@hotmail.co.uk>
2019-07-23 19:49:29 +0000

    Tidy code, added some very basic styling

--- accounts/backends.py
+++ accounts/backends.py
@@ -3,10 +3,8 @@ from django.contrib.auth.models import User
 
 class EmailAuth:
     """Authenticate a user by an email"""
-    
     def authenticate(self, username=None, password=None):
         """Get an instance of User based off the Email"""
-        
         try:
             user = User.objects.get(email=username)
             
@@ -16,11 +14,10 @@ class EmailAuth:
             return None
         except User.DoesNotExist:
             return None
-            
-        
+
+
     def get_user(self, user_id):
         """Used by Django authentication system to get user instance"""
-        
         try:
             user = User.objects.get(pk=user_id)
             
@@ -29,7 +26,3 @@ class EmailAuth:
             return None
         except User.DoesNotExist:
             return None
-        
-        
-            
-        
