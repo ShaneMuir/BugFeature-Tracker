d33ff57180e390d461a67ab3ddf60cfffbd58a97
Shane Muirhead <shane_muirhead@hotmail.co.uk>
2019-07-31 11:53:32 +0000

    Added in upvoting functionality

--- bugs/urls.py
+++ bugs/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-from .views import show_all_bugs, single_bug_view
+from .views import show_all_bugs, single_bug_view, upvote_bug
 
 urlpatterns = [
     url(r'^$', show_all_bugs, name="show_all_bugs"),
-    url(r'^(?P<pk>\d+)/$', single_bug_view, name="single_bug_view")
+    url(r'^(?P<pk>\d+)/$', single_bug_view, name="single_bug_view"),
+    url(r'upvote/(?P<bug_id>[0-9]+)/$', upvote_bug, name='upvote_bug'),
     ]
--- bugs/views.py
+++ bugs/views.py
@@ -1,19 +1,17 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.utils import timezone
-from .models import Bug, BugComment
+from .models import Bug, BugComment, upVotes
 from .forms import BugCommentForm
 
-
 # Create your views here.
-
 def show_all_bugs(request):
     """
     View to show all our bugs on
     one page
     """
-    bugs = Bug.objects.filter(created_date__lte=timezone.now()
-                             ).order_by('-upvotes')
+    bugs = Bug.objects.filter(created_date__lte=timezone.now())
     
     context = {
         'bugs': bugs
@@ -48,10 +46,28 @@ def single_bug_view(request, pk):
     context = {
         'bug' : bug,
         'bug_comment_form': bug_comment_form,
-        'comments': comments
+        'comments': comments,
     }
         
     return render(request, 'single_bug.html', context)
     
 
+@login_required
+def upvote_bug(request, bug_id):
+    """
+    Route to allow users to upvote bugs
     
+    """
+    bug = Bug.objects.get(pk=bug_id)
+    check_user_voted = upVotes.objects.filter(upvote_user=request.user, bug=bug)
+    if not check_user_voted:
+        upvote = upVotes(upvote_user=request.user, bug=bug)
+        upvote.save()
+        bug.bug_upvotes += 1
+        bug.save()
+        messages.success(request, "Bug upvoted", extra_tags="alert-success")
+        return redirect(request.META.get('HTTP_REFERER'))
+    else:
+        messages.error(request, 'Sorry {0} you have already upvoted {1}!'.format(request.user, bug.title), extra_tags="alert-primary")
+        return redirect(request.META.get('HTTP_REFERER'))
+        
