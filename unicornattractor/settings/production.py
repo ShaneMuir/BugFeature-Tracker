042b3cff19530b2efff2f442585a4bad8f3b0ae4
Shane Muirhead <shane_muirhead@hotmail.co.uk>
2019-08-01 14:51:15 +0000

    testing heroku

--- unicornattractor/settings/production.py
+++ unicornattractor/settings/production.py
@@ -20,8 +20,8 @@ ALLOWED_HOSTS = [
 ] 
 
 
-if 'DATEBASE_URL' in os.environ:
-    DATABASES = {
+# if 'DATEBASE_URL' in os.environ:
+DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
         }
 # else:
