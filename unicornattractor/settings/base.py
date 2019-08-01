ea0228f193e61c4f82b25041c90dcc6b6a06572e
Shane Muirhead <shane_muirhead@hotmail.co.uk>
2019-08-01 14:48:41 +0000

    testing heroku

--- unicornattractor/settings/base.py
+++ unicornattractor/settings/base.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import dj_database_url
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -73,15 +74,17 @@ WSGI_APPLICATION = 'unicornattractor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#         }
 
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# # DATABASES = {
+# #     'default': {
+# #         'ENGINE': 'django.db.backends.sqlite3',
+# #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# #     }
+# # }
 
 
 
