007297a1ce87fedea0ffad361a53c1ee239ddb0e
Shane Muirhead <shane_muirhead@hotmail.co.uk>
2019-08-01 14:46:42 +0000

    testing heroku

--- unicornattractor/settings/production.py
+++ unicornattractor/settings/production.py
@@ -24,13 +24,13 @@ if 'DATEBASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
         }
-else:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# else:
+#     DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 STATICFILES_LOCATION = 'static'
