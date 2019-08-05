5f784df8c91a4f7fb61ada5d7594b5764ce1610b
Shane Muirhead <shane_muirhead@hotmail.co.uk>
2019-08-05 13:46:11 +0000

    added some models to our features app

--- features/admin.py
+++ features/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Feature, FeatureComment
 
 # Register your models here.
+class FeatureAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'status', 'creator',
+                    'created_date', 'feature_upvotes']
+    list_filter = ('status',)
+    
+class FeatureCommentAdmin(admin.ModelAdmin):
+    list_display = ['feature', 'comment', 'creator', 'created_date']
+    list_filter = ('feature', 'creator')
+
+admin.site.register(Feature, FeatureAdmin)
+admin.site.register(FeatureComment, FeatureCommentAdmin)
--- features/models.py
+++ features/models.py
@@ -1,4 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 
 # Create your models here.
+class Feature(models.Model):
+    """A feature post"""
+    STATUS_CHOICES = (
+        ('To do', 'To do'),
+        ('In progress', 'In progress'),
+        ('Complete', 'Complete')
+        )
+
+
+    title = models.CharField(max_length=56, blank=False)
+    description = models.TextField(blank=False)
+    status = models.CharField(max_length=20,
+                              choices=STATUS_CHOICES,
+                              default='To do')
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    feature_upvotes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    
+    class Meta:
+        ordering = ['-feature_upvotes']
+    
+    
+    def __str__(self):
+        return self.title
+        
+
+class FeatureComment(models.Model):
+    """Feature Comments"""
+    feature = models.ForeignKey(Feature, on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField(max_length=256, blank=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return '{0}: {1}'.format(self.feature.title, self.creator.username)
