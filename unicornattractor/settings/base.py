4cf904efae4d9592a9fe880480b6ec347309dadd
Shane Muirhead <shane_muirhead@hotmail.co.uk>
2019-08-05 15:05:41 +0000

    Testing Heroku and Travis together

--- unicornattractor/settings/base.py
+++ unicornattractor/settings/base.py
@@ -8,7 +8,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import env
+import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -76,13 +77,17 @@ WSGI_APPLICATION = 'unicornattractor.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 
-DATABASES = {
+if 'DATEBASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        }
+else:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 
 # Password validation
