563e1c7a0be686c1a0f032452bbcdfc96596d907
Shane Muirhead <shane_muirhead@hotmail.co.uk>
2019-08-05 15:22:26 +0000

    Found and fixed type in variable name!

--- unicornattractor/settings/base.py
+++ unicornattractor/settings/base.py
@@ -8,12 +8,12 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env
 import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ''
@@ -77,11 +77,12 @@ WSGI_APPLICATION = 'unicornattractor.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 
-if 'DATEBASE_URL' in os.environ:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
         }
 else:
+    print('no db found')
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
--- unicornattractor/settings/production.py
+++ unicornattractor/settings/production.py
@@ -20,7 +20,7 @@ ALLOWED_HOSTS = [
 ] 
 
 
-if 'DATEBASE_URL' in os.environ:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
         }
