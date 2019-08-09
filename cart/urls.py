56f606ae8b5544c9649d43644e445c42a67a71b2
Shane Muirhead <shane_muirhead@hotmail.co.uk>
2019-08-09 11:56:56 +0000

    Got cart app fully functional

--- cart/urls.py
+++ cart/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import view_cart, add_to_cart, increase_cart, decrease_cart
+from .views import view_cart, add_to_cart, increase_cart, decrease_cart, remove_from_cart
 
 
 
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^add/(?P<id>\d+)/$', add_to_cart, name='add_to_cart'),
     url(r'^increase/(?P<id>\d+)/$', increase_cart, name='increase_cart'),
     url(r'^decrease/(?P<id>\d+)/$', decrease_cart, name='decrease_cart'),
+    url(r'^remove/(?P<id>\d+)/$', remove_from_cart, name='remove_from_cart'),
     ]
--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import login_required
 from features.views import show_all_features
+from django.contrib import messages
 
 # Create your views here.
 @login_required
@@ -13,7 +14,11 @@ def view_cart(request):
 def add_to_cart(request, id):
     """Add a feature to the cart for upvoting"""
     quantity = int(request.POST.get('quantity'))
+    feature_id = request.POST.get('id')
     cart = request.session.get('cart', {})
+    for k,v in cart.items():
+        if k == feature_id:
+            messages.error(request, "Sorry {0}, that feature is already in your cart".format(request.user), extra_tags="alert-primary")
     cart[id] = cart.get(id, quantity)
     request.session['cart'] = cart
     return redirect(request.META.get('HTTP_REFERER'))
@@ -34,4 +39,20 @@ def decrease_cart(request, id):
     if cart[id]:
         cart[id] -= 1
     request.session['cart'] = cart
+    if cart[id] == 0:
+        cart.pop(id)
     return redirect(request.META.get('HTTP_REFERER'))
+    
+   
+def remove_from_cart(request, id):
+    """Remove a feature from the cart view"""
+    feature_id = request.POST.get('id')
+    cart = request.session.get('cart', {})
+    for k,v in cart.items():
+        if feature_id in k:
+            cart.pop(k)
+            request.session['cart'] = cart
+            return redirect(request.META.get('HTTP_REFERER'))
+        else:
+            messages.error(request, 'Sorry {0} , something went wrong.'.format(request.user), extra_tags="alert-primary")
+            return redirect(request.META.get('HTTP_REFERER'))
