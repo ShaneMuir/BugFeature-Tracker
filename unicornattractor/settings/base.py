cc88c23ba02215fe9f709e507775a8f2ef7ae35f
Shane Muirhead <shane_muirhead@hotmail.co.uk>
2019-08-12 09:40:35 +0000

    switched site to test db for testing

--- unicornattractor/settings/base.py
+++ unicornattractor/settings/base.py
@@ -83,13 +83,13 @@ WSGI_APPLICATION = 'unicornattractor.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-        }
-else:
-    print('no db found')
-    DATABASES = {
+# if 'DATABASE_URL' in os.environ:
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#         }
+# else:
+#     print('no db found')
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
