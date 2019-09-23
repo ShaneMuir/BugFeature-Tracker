c32d52d1b925a417473d9bcd353ff1a17ad46a33
Shane Muirhead <shane_muirhead@hotmail.co.uk>
2019-09-23 13:54:25 +0000

    added custom styles onto pygal charts

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -2,7 +2,15 @@ from bugs.models import Bug
 from features.models import Feature
 
 import pygal
-from pygal.style import NeonStyle
+from pygal.style import Style, LightenStyle
+
+dark_lighten_style = LightenStyle('#A9A9A9',
+                                   title_font_size =60,
+                                   tooltip_font_size = 30,
+                                   background = 'transparent',
+                                   font_family = 'Muli',
+                                   legend_font_size	 = 18,
+                                   )
 
 def chart():
     todo = Bug.objects.filter(status="To do").count()
@@ -10,10 +18,11 @@ def chart():
     complete = Bug.objects.filter(status="Complete").count()
     
     pie_chart = pygal.Pie(
-        print_values=True,
-        show_legend=True,
+        print_values=False,
         human_readable=True,
+        show_legend=True,
         fill=True,
+        style = dark_lighten_style,
         )
     pie_chart.title = 'Bugs'
     pie_chart.add('To do', todo)
