c284aaed9ca6f8a344d8d610af4f05aff18c79dc
Shane Muirhead <shane_muirhead@hotmail.co.uk>
2019-09-23 09:32:28 +0000

    Fixed minor bug showing stuff in carts when searching

--- search/views.py
+++ search/views.py
@@ -5,14 +5,10 @@ from features.models import Feature
 # Create your views here.
 def search(request):
     bugs = Bug.objects.filter(title__icontains=request.GET['q'])
-    features = Feature.objects.filter(title__icontains=request.GET['q'], paid=True)
-    bug_count = bugs.count()
-    feature_count = features.count()
+    features = Feature.objects.filter(title__icontains=request.GET['q']).exclude(paid=False)
     
     context = {
         'bugs': bugs,
         'features': features,
-        'bug_count': bug_count,
-        'feature_count': feature_count,
     }
     return render(request, "search.html", context)
