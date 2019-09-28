e5217a1df22e0d1a6c8169191004258624f71122
Shane Muirhead <shane_muirhead@hotmail.co.uk>
2019-09-28 16:03:26 +0000

    Added docstrings to my modules

--- home/tests.py
+++ home/tests.py
@@ -4,7 +4,10 @@ from .views import index
 # Create your tests here.
 
 class TestHomePage(TestCase):
-    
+    """
+    Test that our home back returns the correct
+    status
+    """
     def test_home_page_is_rendered(self):
         url = self.client.get('/')
         self.assertEqual(url.status_code, 200)
--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    """Return our landing page on itinal load"""
     return render(request, 'index.html')
     
     
