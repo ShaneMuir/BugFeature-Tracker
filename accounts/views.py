6f533f493971316d4ea9a1e9061cea50e4b13384
Shane Muirhead <shane_muirhead@hotmail.co.uk>
2019-09-28 18:24:37 +0000

    Added pagination to profile page

--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User
 from accounts.forms import UserLoginForm, UserRegistrationForm
 from bugs.models import Bug
 from features.models import Feature
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 # Create your views here.
 def register(request):
@@ -85,8 +87,23 @@ from graphs.graphs import bug_pie_chart, feature_pie_chart, most_upvoted_bug, mo
 def profile(request):
     """User profile page"""
     user = User.objects.get(email=request.user.email)
-    bugs = Bug.objects.filter(creator=user.id)
-    features = Feature.objects.filter(creator=user.id, paid=True)
+    bug_list = Bug.objects.filter(creator=user.id)
+    feature_list = Feature.objects.filter(creator=user.id, paid=True)
+    page = request.GET.get('page', 1)
+    
+    bug_paginator = Paginator(bug_list, 2)
+    feature_paginator = Paginator(feature_list, 2)
+    
+    try:
+        bugs = bug_paginator.page(page)
+        features = feature_paginator.page(page)
+    except PageNotAnInteger:
+        bugs = bug_paginator.page(1)
+        features = feature_paginator.page(1)
+    except EmptyPage:
+        bugs = bug_paginator.page(paginator.num_pages)
+        features = feature_paginator.page(paginator.num_pages)
+    
     
     context = {
         'profile': user,
